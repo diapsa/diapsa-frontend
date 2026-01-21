1612b1198cd589afedc49c12963d2786ba48b243
LeosterCode <leosternunez@gmail.com>
2026-01-21 12:46:41 -0600

    Improving values section

--- components/organisms/ValuesSection.tsx
+++ components/organisms/ValuesSection.tsx
@@ -1,39 +1,26 @@
+import valuesData from '@/data/values.json';
 export function ValuesSection() {
 
     return (
 
-        <section className="w-full py-16 lg:py-24 border-y-4 border-secondary bg-primary">
-            <div className="sm:px-16 md:px-32">
-                <h3 className="text-center text-4xl">Valores dentro de nuestra empresa</h3>
-                <div className="w-full grid sm:grid-cols-2 md:grid-cols-4 sm:gap-16 gap-8 justify-items-center mt-16">
-
-                    {[{
-                        value: "Responsabilidad"
-                    },
-                    {
-                        value: "Compromiso",
-                    },
-                    {
-                        value: "Calidad",
-                    },
-                    {
-                        value: "Trabajo en equipo",
-                    }
-                    ].map((card) => (
-
-                        <div key={card.value} className="w-28 h-28 bg-secondary rotate-45 flex justify-center items-center shadow-[0_0_50px_rgba(255,255,255,0.35)]">
-                            <div className="-rotate-45 text-lg text-center font-medium text-black">
-                                {card.value}
-                            </div>
-
+        <section className="w-full py-14 lg:py-18 border-y-4 border-secondary">
+            <div className="sm:px-16 md:px-16 flex flex-col justify-center items-center gap-6">
+                <h3 className='font-bold text-center text-secondary'>Nuestra identidiad</h3>
+                <h3 className="text-center text-4xl font-extrabold">Valores dentro de nuestra empresa</h3>
+                <div className='border-2  border-secondary md:w-[10%] justify-items-center' />
+                <p className='w-3xl text-center text-lg'>Nuestros principios fundamentales guian cada decisión y acción, asegurando la excelencia operativa
+                    y el compromiso con nuestros clientes industriales</p>
+                <div className="w-full grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 sm:gap-16 gap-8 justify-items-center mt-6">
+                    {valuesData.values.map((value) => (
+                        <div className='flex flex-col items-center gap-6'>
+                            <div key={value.name} className="w-8 h-8 bg-secondary rotate-45 flex justify-center items-center shadow-[0_0_50px_rgba(255,255,255,0.35)]" />
+                            <p className='text-center text-xl font-bold'>{value.name}</p>
+                            <p className='text-center '>{value.description}</p>
                         </div>
                     ))}
-
-                </div>
-                <div>
-
                 </div>
             </div>
         </section>
+
     )
 }
