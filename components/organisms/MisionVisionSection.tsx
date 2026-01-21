a02b06280edcb1722e6da8d7c8ca6d04e4a95686
LeosterCode <leosternunez@gmail.com>
2026-01-21 12:47:40 -0600

    Improving Mision and vision section

--- components/organisms/MisionVisionSection.tsx
+++ components/organisms/MisionVisionSection.tsx
@@ -1,39 +1,53 @@
 import Image from "next/image"
 export function MisionVisionSection() {
     return (
-        <section className="w-full bg-white py-16 lg:py-16">
+        <section className="w-full bg-white py-10 space-y-4">
+            <div className="space-y-2 mb-16">
+                <div className="flex flex-col items-center">
+                    <p className="bg-secondary/10 p-2 text-secondary rounded-lg font-bold">Exelencia operativa</p>
+                </div>
+                <h2 className="text-black text-4xl font-extrabold text-center">Comprometidos con la  <span className="text-secondary">Confiabilidad</span></h2>
+                <p className="text-black text-center">Somos lideres en mantenimiento predicivo y monitoreo de condiciones, asegurando que su planta opera con la maxima eficiencia y seguridad</p>
+
+            </div>
             {/* Misión */}
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-10 items-center px-8 lg:px-16 mb-16">
-                <div className="px-6">
-                    <h3 className="text-2xl text-gray-600 font-bold border-b-4 border-secondary mb-4 pb-2">Nuestra misión</h3>
-                    <p className="text-center text-lg italic text-gray-700 leading-relaxed">
+            <div className="grid grid-cols-1 md:grid-cols-2 md:gap-16 items-center lg:px-28 min-h-[50vh]">
+                <div className="md:py-8">
+                    <h3 className="font-semibold text-2xl text-gray-600 border-b-4 border-secondary mb-4 pb-2">Nuestra misión</h3>
+                    <p className="text-lg italic text-gray-700 leading-relaxed">
                         "Ser líderes en las disciplinas que
                         desarrollamos a través de un servicio de calidad, precisión y rapidez, beneficiando a
                         nuestros clientes, incrementando la confiabilidad de los equipos y la productividad de
                         la planta."
                     </p>
                 </div>
-                <div className="relative w-full h-64 rounded-lg overflow-hidden shadow-md hidden md:block ">
-                    <Image
-                        src="/images/metodologia/metodologia-1.jpg"
-                        fill
-                        className="hidden md:block object-cover"
-                        alt="Representación de la misión de la empresa" />
+                <div className="relative w-full h-full rounded-4xl border-e-4 border-t-4 border-secondary hidden md:block p-4">
+                    <div className="relative w-full h-full overflow-hidden rounded-2xl">
+                        <Image
+                            src="/images/metodologia/metodologia-1.jpg"
+                            fill
+                            className="object-cover"
+                            alt="Representación de la misión de la empresa"
+                        />
+                    </div>
                 </div>
             </div>
 
             {/* Visión */}
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-10 items-center px-8 lg:px-16 ">
-                <div className="relative w-full h-64 rounded-lg overflow-hidden shadow-md xs:order-2 hidden md:block ">
-                    <Image
-                        src="/images/metodologia/metodologia-1.jpg"
-                        fill
-                        className="hidden md:block object-cover"
-                        alt="Representación de la visión de la empresa" />
+            <div className="grid grid-cols-1 md:grid-cols-2 md:gap-16 items-center px-8 lg:px-28 min-h-[50vh]">
+                <div className="relative w-full h-full rounded-4xl border-b-4 border-s-4 border-primary hidden md:block p-4">
+                    <div className="relative w-full h-full overflow-hidden rounded-2xl">
+                        <Image
+                            src="/images/metodologia/metodologia-1.jpg"
+                            fill
+                            className="object-cover"
+                            alt="Representación de la misión de la empresa"
+                        />
+                    </div>
                 </div>
-                <div className="px-6 xs:order-1">
-                    <h3 className="text-2xl text-gray-600 font-bold border-b-4 border-secondary mb-4 pb-2">Nuestra visión</h3>
-                    <p className="text-center text-lg italic text-gray-700 leading-relaxed">
+                <div className="md:py-16">
+                    <h3 className="font-semibold text-2xl text-gray-600  border-b-4 border-primary mb-4 pb-2">Nuestra visión</h3>
+                    <p className="text-lg italic text-gray-700 leading-relaxed">
                         "Elevar la confiabilidad y seguridad de las plantas a través
                         del monitoreo de condición del estado de la maquinaria mediante herramientas
                         predictivas, desarrollando los servicios con eficiencia y calidad, comprometidos como
