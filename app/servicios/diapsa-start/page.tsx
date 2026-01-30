23416c4b924b917434669d3d90f86a01acac5329
LeosterCode <leosternunez@gmail.com>
2026-01-30 14:09:23 -0600

    fix: card padding adjustment

--- app/servicios/diapsa-start/page.tsx
+++ app/servicios/diapsa-start/page.tsx
@@ -1,10 +1,6 @@
 import PageHeader from "@/components/organisms/PageHeader"
-import PlusCircleIcon from '@/components/atoms/icons/PlusCircleIcon'
-import DoubleCircleIcon from "@/components/atoms/icons/DoubleCircleIcon"
+import { PlusCircleIcon, ChartIcon, ChartIcon2, WindowIcon, DoubleCircleIcon } from '@/components/atoms/icons/'
 import Button from "@/components/atoms/Button"
-import WindowIcon from "@/components/atoms/icons/WindowIcon"
-import ChartIcon from "@/components/atoms/icons/ChartIcon"
-import ChartIcon2 from "@/components/atoms/icons/ChartIcon2"
 
 export default function DiapsaStart() {
     return (
@@ -17,12 +13,13 @@ export default function DiapsaStart() {
             <section className="w-full  py-16 lg:py-12 space-y-32">
                 <div className="space-y-6">
 
-                    <div className="px-2 lg:px-10">
-                        <h2 className="text-6xl font-extrabold">Aprender haciendo. <br />
-                            Construir capacidades.</h2>
-                        <h3 className="text-5xl italic text-secondary font-extrabold">Tomar control de los activos</h3>
+                    <div className="px-2 lg:px-10 space-y-2">
+                        <h2 className="text-3xl lg:text-6xl font-extrabold">Aprender haciendo. <br /> </h2>
+
+                        <h2 className="text-3xl lg:text-6xl font-extrabold"> Construir capacidades.</h2>
+                        <h3 className="text-2xl lg:text-7xl italic text-secondary font-extrabold">Tomar control de los activos</h3>
                     </div>
-                    <div className="grid grid-cols-1 lg:grid-cols-2 justify-items-center w-full px-2 lg:px-24">
+                    <div className="grid grid-cols-1 lg:grid-cols-2 justify-items-center w-full px-2 lg:px-24 space-y-4">
                         <p className="text-gray-300 text-xl">
                             El programa DIAPSA Start trasciende las simple instalación tecnica. Es una transformación
                             estructural en la cultura de mantenimiento, diseñada para empresas que demandan una estrategia
@@ -36,7 +33,7 @@ export default function DiapsaStart() {
                 </div>
 
                 <div className="px-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 justify-items-center gap-6">
-                    <div className="relative px-8 md:px-12 lg:px-16 py-16 md:py-24 lg:py-32 space-y-6 bg-primary aspect-8/12 w-full overflow-hidden">
+                    <div className="relative px-8 md:px-12 lg:px-16 py-16 md:py-18 lg:py-18 space-y-6 bg-primary aspect-8/12 w-full overflow-hidden">
                         <DoubleCircleIcon className="text-secondary w-8 h-8" />
                         <p className="text-secondary font-bold">Etapa 01</p>
                         <h3 className="text-xl font-bold">Formación técnica aplicada</h3>
@@ -47,7 +44,7 @@ export default function DiapsaStart() {
                             1
                         </div>
                     </div>
-                    <div className="relative px-8 md:px-12 lg:px-16 py-16 md:py-24 lg:py-32 space-y-6 bg-primary border-4 border-secondary aspect-8/12 w-full overflow-hidden">
+                    <div className="relative px-8 md:px-12 lg:px-16 py-16 md:py-18 lg:py-18 space-y-6 bg-primary border-4 border-secondary aspect-8/12 w-full overflow-hidden">
                         <WindowIcon className="text-secondary w-8 h-8" />
                         <p className="text-secondary font-bold">Etapa 02 (core)</p>
                         <h3 className="text-xl font-bold">Diagnóstico situacional</h3>
@@ -61,7 +58,7 @@ export default function DiapsaStart() {
                             2
                         </div>
                     </div>
-                    <div className="relative px-8 md:px-12 lg:px-16 py-16 md:py-24 lg:py-32 space-y-6 bg-primary aspect-8/12 w-full overflow-hidden">
+                    <div className="relative px-8 md:px-12 lg:px-16 py-16 md:py-18 lg:py-18 space-y-6 bg-primary aspect-8/12 w-full overflow-hidden">
                         <ChartIcon className="text-secondary w-8 h-8" />
                         <p className="text-secondary font-bold">Etapa 03</p>
                         <h3 className="text-xl font-bold">Mediciones</h3>
@@ -73,7 +70,7 @@ export default function DiapsaStart() {
                             3
                         </div>
                     </div>
-                    <div className="relative px-8 md:px-12 lg:px-16 py-16 md:py-24 lg:py-32 space-y-6 bg-primary aspect-8/12 w-full overflow-hidden">
+                    <div className="relative px-8 md:px-12 lg:px-16 py-16 md:py-18 lg:py-18 space-y-6 bg-primary aspect-8/12 w-full overflow-hidden">
                         <ChartIcon2 className="text-secondary w-8 h-8" />
                         <p className="text-secondary font-bold">Etapa 04</p>
                         <h3 className="text-xl font-bold">Gestión de datos</h3>
@@ -86,17 +83,17 @@ export default function DiapsaStart() {
                         </div>
                     </div>
                 </div>
-
+                {/* 
                 <div className="px-2 lg:px-24">
 
-                    <div className="border-l-4 border-secondary p-8 flex justify-between bg-primary/90">
+                    <div className="border-l-4 border-secondary p-8 flex flex-col lg:flex-row justify-between bg-primary/90">
                         <div>
                             <p className="text-2xl font-bold">¿Listo para evolucionar su mantenimiento?</p>
                             <p className="text-gray-300">Nuestros expertos estan listos para realizar un pre-diagnóstico de sus activos</p>
                         </div>
                         <Button variant="secondary">AGENDAR CONSULTORÍA</Button>
                     </div>
-                </div>
+                </div> */}
 
             </section>
         </main>
