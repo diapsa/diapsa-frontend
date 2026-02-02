20c9381c5c43d8af57d1249c4561b8cac912fd22
Alfredo Galvan <chencho.backup@gmail.com>
2026-02-02 13:26:28 -0600

    feat: apu base url

--- lib/api/config.ts
+++ lib/api/config.ts
@@ -4,7 +4,7 @@
  */
 
 export const API_BASE_URL = 
-  process.env.NEXT_PUBLIC_API_BASE_URL;
+  process.env.NEXT_PUBLIC_API_BASE_URL || 'https://cms.grupodiapsa.com.mx/api/v1';
 
 export const API_TIMEOUT = 
   parseInt(process.env.NEXT_PUBLIC_API_TIMEOUT || '10000', 10);
