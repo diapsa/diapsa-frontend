f97a419da9701513fa4fff8f053159314e5cdbf3
LeosterCode <leosternunez@gmail.com>
2026-02-20 09:09:57 -0600

    gas detection technology section

---
+++ components/organisms/GasDetectionTechnology.tsx
@@ -0,0 +1,9 @@
+export default function GasDetectionTechnology() {
+    return (
+        <section className="bg-white text-black p-10">
+            <h2>La tecnologia que usamos</h2>
+
+
+        </section>
+    )
+}
